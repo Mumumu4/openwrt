b3d2f726b9ef49cbdaeaaf57517cbbbed1395b70
Mumumu4 <30195921+Mumumu4@users.noreply.github.com>
2024-04-02 19:44:55 -0400

    A nasty workaround to allow use official mirror's kmods since I'm not changed the kernel config too much, it should work?

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -118,7 +118,7 @@ define Kernel/Configure/Default
 		cp $(LINUX_DIR)/.config.set $(LINUX_DIR)/.config.prev; \
 	}
 	$(_SINGLE) [ -d $(LINUX_DIR)/user_headers ] || $(KERNEL_MAKE) $(if $(findstring uml,$(BOARD)),ARCH=$(ARCH)) INSTALL_HDR_PATH=$(LINUX_DIR)/user_headers headers_install
-	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)/.vermagic
+	wget https://downloads.openwrt.org/releases/23.05.3/targets/ath79/nand/openwrt-23.05.3-ath79-nand.manifest -qO-|grep -oP 'kernel -.*-\K.*' > $(LINUX_DIR)/.vermagic
 endef
 
 define Kernel/Configure/Initramfs
